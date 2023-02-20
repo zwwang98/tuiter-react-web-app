0de9e40ca3e2a1071c59bd2c4d3f325bf47d6d44
ziweiwang98 <wang.ziwei2@northeastern.edu>
2023-02-19 21:42:05 -0800

    Finished 3.1

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,9 +1,11 @@
 import Nav from "../labs/a6/nav";
+import NavigationSidebar from "./navigation-sidebar";
 
 function Tuiter() {
   return (
     <div>
       <Nav />
+      <NavigationSidebar active="home" />
       <h1>Tuiter</h1>
     </div>
   );
