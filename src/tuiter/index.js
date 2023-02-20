07b2569d154b25e7abdc50bfdc18337ea117d309
ziweiwang98 <wang.ziwei2@northeastern.edu>
2023-02-20 00:53:10 -0800

    Finished 3.3

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,12 +1,13 @@
 import Nav from "../labs/a6/nav";
 import NavigationSidebar from "./navigation-sidebar";
-import WhoToFollowListItem from "./who-to-follow-list/who-to-follow-list-item";
+import WhoToFollowList from "./who-to-follow-list";
 
 function Tuiter() {
   return (
     <div>
       <Nav />
       <NavigationSidebar active="home" />
+      <WhoToFollowList />
       <h1>Tuiter</h1>
     </div>
   );
