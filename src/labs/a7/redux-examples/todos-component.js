3b45fa0395a36ced914a4b6028931bc9425306eb
ziweiwang98 <wang.ziwei2@northeastern.edu>
2023-03-14 16:09:46 -0700

    Finish lab 2.6

--- src/labs/a7/redux-examples/todos-component.js
+++ src/labs/a7/redux-examples/todos-component.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 const Todos = () => {
   const todos = useSelector((state) => state.todos);
+  const [todo, setTodo] = useState({ do: "" });
+  const todoChangeHandler = (event) => {
+    const doValue = event.target.value;
+    const newTodo = {
+      do: doValue,
+    };
+    setTodo(newTodo);
+  };
+
   return (
     <>
       <h3>Todos</h3>
       <ul className="list-group">
+        <li className="list-group-item">
+          <input
+            onChange={todoChangeHandler}
+            value={todo.do}
+            className="form-control"
+          />
+        </li>
         {todos.map((todo) => (
           <li className="list-group-item">
             <p>id: {todo._id}</p>
