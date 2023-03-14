dc056819c3d11639bd4ee2b8f3cebdb00295c8bd
ziweiwang98 <wang.ziwei2@northeastern.edu>
2023-03-14 16:17:31 -0700

    Finishe lab 2.7

--- src/labs/a7/redux-examples/reducers/todos-reducer.js
+++ src/labs/a7/redux-examples/reducers/todos-reducer.js
@@ -16,6 +16,17 @@ const initialTodos = [
 const todosSlice = createSlice({
   name: "todos",
   initialState: initialTodos,
+  reducers: {
+    addTodo: (state, action) => {
+      state.push({
+        _id: new Date().getTime(),
+        do: action.payload.do,
+        done: false,
+      });
+    },
+  },
 });
+
+export const { addTodo } = todosSlice.actions;
 
 export default todosSlice.reducer;
--- src/labs/a7/redux-examples/todos-component.js
+++ src/labs/a7/redux-examples/todos-component.js
@@ -1,8 +1,15 @@
 import React, { useState } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { addTodo } from "./reducers/todos-reducer";
+
 const Todos = () => {
   const todos = useSelector((state) => state.todos);
   const [todo, setTodo] = useState({ do: "" });
+  const dispatch = useDispatch();
+  const createTodoClickHandler = () => {
+    dispatch(addTodo(todo));
+  };
+
   const todoChangeHandler = (event) => {
     const doValue = event.target.value;
     const newTodo = {
@@ -16,6 +23,13 @@ const Todos = () => {
       <h3>Todos</h3>
       <ul className="list-group">
         <li className="list-group-item">
+          <button
+            onClick={createTodoClickHandler}
+            className="btn btn-primary w-25 
+                          float-end"
+          >
+            Create
+          </button>
           <input
             onChange={todoChangeHandler}
             value={todo.do}
